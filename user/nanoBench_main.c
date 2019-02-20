52c7c221411f91637f159c0232127c17afdb703c
Andreas Abel <abel@cs.uni-saarland.de>
2019-02-20 16:02:56 +0100

    adding license

--- user/nanoBench_main.c
+++ user/nanoBench_main.c
@@ -1,3 +1,14 @@
+// nanoBench
+//   
+// Copyright (C) 2019 Andreas Abel
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of version 3 of the GNU Affero General Public License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
