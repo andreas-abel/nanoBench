d182d44406eea5ab1213d2c7e42f8c91a08490e2
Andreas Abel <abel@cs.uni-saarland.de>
2019-09-24 14:32:20 +0200

    single core mode

---
+++ single-core-mode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+for cpu in /sys/devices/system/cpu/cpu[1-9]*; do
+    echo 0 > "$cpu/online"
+done
