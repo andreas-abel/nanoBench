08e351b7fb158a4b44efee4126e05ad9ebbe5cf5
Andreas Abel <mail@andreasabel.de>
2025-05-18 16:22:49 +0200

    configs for EMR, MTL, and ARL

--- configs/convertIntelJSON.py
+++ configs/convertIntelJSON.py
@@ -8,9 +8,10 @@ parser.add_argument('url', help='URL of JSON file')
 parser.add_argument('-offcore', help='Convert offcore events', action='store_true')
 args = parser.parse_args()
 
-print('# Based on ' + args.url)
+json = requests.get(args.url).json()
+print(f'# Based on {args.url} (Version: {json["Header"]["Version"]})')
 
-mapFile = requests.get('https://download.01.org/perfmon/mapfile.csv')
+mapFile = requests.get('https://raw.githubusercontent.com/intel/perfmon/refs/heads/main/mapfile.csv')
 famMod = []
 for l in mapFile.iter_lines():
     fields = l.decode().split(',')
@@ -19,13 +20,12 @@ for l in mapFile.iter_lines():
 if famMod:
     print('# Applies to processors with family-model in {' + str(famMod).replace("'", '')[1:-1] + '}')
 
-json = requests.get(args.url).json()
-allCtrs = max([ev['Counter'] for ev in json if not 'Fixed' in ev['Counter']], key=len)
+allCtrs = max([ev['Counter'] for ev in json['Events'] if not 'Fixed' in ev['Counter']], key=len)
 if '0,1,2,3' in allCtrs:
     allCtrs = '0,1,2,3' # nanoBench does not use counters >= 4
 
 evDescriptions = []
-for ev in sorted(json, key=lambda x: (x['EventCode'].upper(), x['UMask'].upper())):
+for ev in sorted(json['Events'], key=lambda x: (x['EventCode'].upper(), x['UMask'].upper())):
     if ('Fixed' in ev['Counter']) or (ev['Counter'] in ['32', '33', '34', '35']):
         continue
     if ev.get('Deprecated') == '1':
